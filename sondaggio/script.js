542f7c5ddae276ba5e09ea376c9c3188b15dc2ef
domi <ltdomenicosg@gmail.com>
2020-12-04 14:35:48 +0100

    add 404 and homepage
    + 404
    + home

--- sondaggio/script.js
+++ sondaggio/script.js
@@ -1,9 +1,14 @@
 console.log("ciaone");
+var n_link = 0;
+var form_minore = 0;
 
 
 var risposte = new Array();
 var url = document.URL;
+
 var key = url.split("?")[1];
+
+
 
 
 
@@ -13,9 +18,11 @@ var key = url.split("?")[1];
 var prop = key;
 
 var link = {
-    "prop1": [ //proprietario e chiave nel url
+    "tende_al_grafene": [ //proprietario e chiave nel url
         "https://docs.google.com/forms/d/1DSzHG0GpTR4diA8cLeFuQG3lAG-MEJ4mQMKCS881Yfs", // link "pulito" senza viewform o viwe analytics ecc
         "https://docs.google.com/forms/u/0/d/e/1FAIpQLSeM87gTSs3tU8c_PJ2RaxWQfzp1emUc3kR-hbvJAXX4bPKpsw",
+        "https://docs.google.com/forms/d/1QW63MnJ6_H1bdTcXGUdGJxQVn3lozM-IH0hc5SbLw3w",
+        "https://docs.google.com/forms/d/1HD-MWmGgAmCDC4CGBoy0IoxBBMGM-LnGQZuJWcHvH5E",
 
     ],
     "prop2": [
@@ -28,57 +35,84 @@ var link = {
 // AGGIUNGERE SE KEY NON ESISTE 404
 // ordinare file
 
-var n_link = 0;
-var form_minore = 0;
+
+
+
+
+
+
+
 
 function f() {
 
-    richieste();
+    if (key && !link[key]) {        // se key è definita && link[key] è indefinita  = se key esiste ma non c'è nell'elenco
+
+        console.log("sondaggio non trovato 404")
+        nontrovato();
+    }
+    if (!key && !link[key]) {
+        console.log("HOME");
+        home();
+
+    }
+    if (key && link[key]) {
+        richieste();
+
+
+    }
+
 
 }
 function richieste() {
-    
 
 
-        for (let i = 0; i < (link[prop].length); i++) {
+
+    for (let i = 0; i < (link[prop].length); i++) {
 
 
-            $.ajaxSetup({
-                scriptCharset: "utf-8", //or "ISO-8859-1",
-                contentType: "application/json; charset=utf-8"
+        $.ajaxSetup({
+            scriptCharset: "utf-8", //or "ISO-8859-1",
+            contentType: "application/json; charset=utf-8"
+        });
+        var linkanalitycs = link[prop][i] + "/viewanalytics";
+        $.getJSON("https://api.allorigins.win/get?url=" +
+            encodeURIComponent(linkanalitycs) + "&callback=?",
+            function (data) {
+
+                var c = $(data.contents).find("script")[0].innerHTML;
+
+
+                eval(c);
+
+                risposte[i] = ANALYTICS_LOAD_DATA_[5];
+
+                // console.log(check());
+                if (check()) {  // se l'array di risposte è pieno 
+                    mostra_form()
+                }
             });
-            var linkanalitycs = link[prop][i] + "/viewanalytics";
-            $.getJSON("https://api.allorigins.win/get?url=" +
-                encodeURIComponent(linkanalitycs) + "&callback=?",
-                function (data) {
-
-                    var c = $(data.contents).find("script")[0].innerHTML;
 
 
-                    eval(c);
-
-                    risposte[i] = ANALYTICS_LOAD_DATA_[5];
-                    console.log(risposte);
-                    console.log(check());
-                    
-                    if (check()) {  // se l'array di risposte è pieno 
-                        mostra_form()
-                    }
-                });
+        ;
 
 
-            ;
-
-
-        }
-    
     }
 
+}
+
 function mostra_form() {
+    const frame = document.createElement("IFRAME");   // Create a <button> element
+    frame.id="form" 
+    frame.width="100%" 
+    frame.height="-webkit-fill-available" 
+    frame.frameborder="0" 
+    frame.marginheight="0" 
+    frame.marginwidth="0"
+    document.body.appendChild(frame)
 
-    index_form_minore = minore(risposte); 
+    index_form_minore = minore(risposte);
 
-    
+
 
     document.getElementById("form").src = link[prop][index_form_minore] + "/viewform?embedded=true";
     document.getElementById("form").style["height"] = "-webkit-fill-available";
@@ -87,8 +121,7 @@ function mostra_form() {
 function minore(a) {
 
     var minimo = Math.min.apply(0, a); // trova il minor numero di risposte
-    console.log(a);
-    console.log(minimo);
+
 
     for (let i = 0; i < a.length; i++) {
         if (a[i] == minimo) {
@@ -100,13 +133,63 @@ function minore(a) {
     }
 }
 function check() {
-    
 
-for (let i = 0; i < risposte.length; i++) { // per ogni elemento controlla che non sia vuoto e che il numero di elementi sia al completo
-    if( !Number.isInteger(risposte[i] || risposte.length != link[prop].length)){
-        return false;
+
+    for (let i = 0; i < risposte.length; i++) { // per ogni elemento controlla che non sia vuoto e che il numero di elementi sia al completo
+        if (!Number.isInteger(risposte[i] || risposte.length != link[prop].length)) {
+            return false;
+        }
+        return true
+
     }
-    return true
-    
 }
+
+function home() {
+    const h1 = document.createElement("H1"); 
+    h1.innerHTML = "Elenco sondaggi";
+    document.body.appendChild(h1);
+    document.body.appendChild(document.createElement("BR"));
+    document.body.appendChild(document.createElement("BR"));
+
+
+    for (let i = 0; i < Object.keys(link).length; i++) {
+
+        var key = Object.keys(link)[i];
+        
+        const a = document.createElement("A");   
+        const p = document.createElement("P");  
+        a.href = "/?"+key;
+        p.innerHTML = key.replaceAll("_", " ");                  
+
+        document.body.appendChild(a).appendChild(p);
+
+
+    }
+}
+function nontrovato() {
+    document.body.appendChild(document.createElement("BR"));
+
+    const h2 = document.createElement("H2"); 
+    h2.innerHTML = "404";
+    document.body.appendChild(h2);
+    const h1 = document.createElement("H1"); 
+    h1.innerHTML = "Sondaggio non trovato";
+    document.body.appendChild(h1);
+    document.body.style["text-align"] =  "center";
+    document.body.style["background-color"] =  "#ff2f50";
+    h1.style["color"] = "#ffffff";
+    h2.style["color"] = "#ffffff";
+
+    document.body.appendChild(document.createElement("BR"));
+    document.body.appendChild(document.createElement("BR"));
+
+    const a = document.createElement("A");   
+    const p = document.createElement("P");  
+    p.innerHTML = "Vai alla home";
+
+    a.href = "../";
+    document.body.appendChild(a).appendChild(p);
+    a.style["color"] = "#ffffff";
+
+
 }
